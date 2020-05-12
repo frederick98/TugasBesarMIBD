0ab5268ad3af5e501f65e36258128b855eca5b31
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2020-05-13 06:16:42 +0700

    SQL Data + view admin done

--- model/sqlData/create table n dummy data.sql
+++ model/sqlData/create table n dummy data.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2020 at 08:57 PM
+-- Generation Time: May 13, 2020 at 01:14 AM
 -- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.3
+-- PHP Version: 7.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -38,7 +38,7 @@ CREATE TABLE `config` (
 --
 
 INSERT INTO `config` (`minReview`, `rating`) VALUES
-(8, 6);
+(3, 5);
 
 -- --------------------------------------------------------
 
@@ -58,7 +58,14 @@ CREATE TABLE `dibuatdi` (
 INSERT INTO `dibuatdi` (`idRamen`, `idRestoran`) VALUES
 (1, 1),
 (2, 1),
-(3, 1);
+(3, 1),
+(4, 2),
+(5, 2),
+(6, 2),
+(7, 3),
+(8, 3),
+(9, 3),
+(10, 3);
 
 -- --------------------------------------------------------
 
@@ -124,7 +131,21 @@ INSERT INTO `pengguna` (`idUser`, `username`, `password`, `nama`, `email`, `kate
 (3, 'suneo27', 'suneo27', 'Thomas Christian', 'suneogiantnobita@blabla.com', 1),
 (4, 'elle787', 'elle787', 'Elle', 'elle787@mail.com', 1),
 (5, 'verra89', 'verra89', 'Verra Tan', 'verra89@gmail.com', 1),
-(6, 'gerald2009', 'gerald2009', 'Gerald', 'gerald2009@mail.com', 1);
+(6, 'gerald2009', 'gerald2009', 'Gerald', 'gerald2009@mail.com', 1),
+(7, 'Nairobiii', 'Nairobiii', 'Nairobiii', 'Nairobiii@gmail.com', 1),
+(8, 'Jevalesta99', 'Jevalesta99', 'Jevalesta', 'Jevalesta99@gmail.com', 1),
+(9, 'Helsinki_88', 'Helsinki_88', 'Helsinki', 'Helsinki_88@gmail.com', 1),
+(10, 'YulianaC_1998', 'YulianaC_1998', 'Yuliana', 'YulianaC_1998@gmail.com', 1),
+(11, 'Bo098', 'Bo098', 'Bobby', 'Bo098@gmail.com', 1),
+(12, '1208_Keynandra', '1208_Keynandra', 'Nandra', '1208_Keynandra@gmail.com', 1),
+(13, 'Zeinan77', 'Zeinan77', 'Zeinan', 'Zeinan77@gmail.com', 1),
+(14, 'Bebebebe', 'Bebebebe', 'Bebe', 'Bebebebe@gmail.com', 1),
+(15, 'Ilvianti78', 'Ilvianti78', 'Livia', 'Ilvianti78@gmail.com', 1),
+(16, 'Christianty99', 'Christianty99', 'Christian', 'Christianty99@gmail.com', 1),
+(17, 'Itsmestella', 'Itsmestella', 'Stella', 'Itsmestella@gmail.com', 1),
+(18, 'Fufi_777', 'Fufi_777', 'Fufi', 'Fufi_777@gmail.com', 1),
+(19, 'Raymund99', 'Raymund99', 'Raymond', 'raymund99@gmail.com', 1),
+(20, 'Doniq99', 'Doniq99', 'Doni', 'doniq99@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -145,7 +166,14 @@ CREATE TABLE `ramen` (
 INSERT INTO `ramen` (`idRamen`, `namaRamen`, `harga`) VALUES
 (1, 'Chicken TantanMen', 25000),
 (2, 'Pork TantanMen', 28000),
-(3, 'Chicken Shoyu', 22000);
+(3, 'Chicken Shoyu', 22000),
+(4, 'Tonkotsu Ramen', 40000),
+(5, 'ToriShio Ramen', 30000),
+(6, 'Tori Shoyu Ramen', 28000),
+(7, 'Curry Ramen', 27000),
+(8, 'Shoyu Ramen', 20000),
+(9, 'Shio Ramen', 30000),
+(10, 'Spicy MisoRamen', 25000);
 
 -- --------------------------------------------------------
 
@@ -165,7 +193,10 @@ CREATE TABLE `restoran` (
 
 INSERT INTO `restoran` (`idRestoran`, `idKota`, `namaRestoran`) VALUES
 (1, 1, 'Ippudo'),
-(2, 1, 'Menya Sakura');
+(2, 1, 'Menya Sakura'),
+(3, 1, 'Nanami Ramen'),
+(4, 1, 'Seirock Ya'),
+(5, 2, 'Gokana Ramen & Teppan');
 
 -- --------------------------------------------------------
 
@@ -189,7 +220,14 @@ CREATE TABLE `review` (
 INSERT INTO `review` (`idReview`, `idUser`, `idRamen`, `review`, `tglReview`, `rating`) VALUES
 (1, 4, 1, 'gila enak banget!', '2020-05-04 12:20:22', 9.5),
 (2, 5, 2, 'lumayan memuaskan!', '2020-05-03 16:39:33', 8.5),
-(3, 6, 3, 'sesuai dengan harganya!', '2020-05-02 16:26:39', 7);
+(3, 6, 3, 'sesuai dengan harganya!', '2020-05-02 16:26:39', 7),
+(4, 7, 4, 'Wahhh enak sekali ini Ramen. Juara lah', '2020-05-10 06:57:00', 9),
+(5, 8, 5, 'Mantep nih Ramen Guys', '2020-05-10 12:00:00', 8),
+(6, 9, 6, 'Rekomend banget nih ramen sis dan bro', '2020-05-09 08:00:00', 8.5),
+(7, 10, 7, 'Rekomend banget nih', '2020-05-12 12:30:00', 7),
+(8, 11, 8, 'Yah lumayan lah', '2020-05-06 10:00:00', 5),
+(9, 12, 9, 'Ga enak guys rasanya hambar banget kaya minum air', '2020-05-12 06:00:00', 4),
+(10, 13, 10, 'Cukup enak, Asin nya pas dan rasanya lumayan enak', '2020-05-11 10:00:00', 7);
 
 --
 -- Indexes for dumped tables
@@ -262,25 +300,25 @@ ALTER TABLE `negara`
 -- AUTO_INCREMENT for table `pengguna`
 --
 ALTER TABLE `pengguna`
-  MODIFY `idUser` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idUser` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `ramen`
 --
 ALTER TABLE `ramen`
-  MODIFY `idRamen` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idRamen` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `restoran`
 --
 ALTER TABLE `restoran`
-  MODIFY `idRestoran` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idRestoran` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `idReview` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idReview` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
